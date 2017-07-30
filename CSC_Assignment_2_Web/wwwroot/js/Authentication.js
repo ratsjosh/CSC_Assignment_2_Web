12d0483e1d39f2de087701efd0f0095b81fd5825
ratsjosh <ratsjosh@outlook.com>
2017-07-31 00:13:58 +0800

    Source: _Layout: Fix navigation bar

---
+++ CSC_Assignment_2_Web/wwwroot/js/Authentication.js
@@ -0,0 +1,40 @@
+﻿function checkAccessToken(callback) {
+    var accessToken = sessionStorage.getItem('accessToken');
+    if (accessToken == null)
+        return false;
+    var entityData = {
+        "AccessToken": accessToken
+    };
+    $.ajax({
+        url: properties.hostConnectionString + '/Account/AuthenticateJwtToken',
+        type: 'POST',
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8",  
+        data: JSON.stringify(entityData),
+        success: function (responseData, textStatus, jqXHR) {
+            callback(textStatus);
+        },
+        error: function (responseData, textStatus, jqXHR) {
+            callback(textStatus);
+        }
+    });
+}
+
+function getLogInUser(email, callback) {
+    $.ajax({
+        url: properties.hostLocalString + '/Account/GetUserByEmailAsync?email=' + email,
+        type: 'GET',
+        crossDomain: true,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        headers: {
+            'Authorization': 'Bearer '
+            + sessionStorage.getItem("accessToken")
+        },
+        success: function (responseData, textStatus, jqXHR) {
+            callback(responseData);
+        },
+        error: function (responseData, textStatus, jqXHR) {
+            callback(textStatus);
+        }
+    });
+}
