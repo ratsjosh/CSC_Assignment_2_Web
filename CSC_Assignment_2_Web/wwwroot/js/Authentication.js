7110f8619cff69b9dc578c00173c50e89dd3f676
Joshua Yap <ratjosh@outlook.com>
2017-08-07 21:15:34 +0800

    Implement Cookie

--- CSC_Assignment_2_Web/wwwroot/js/Authentication.js
+++ CSC_Assignment_2_Web/wwwroot/js/Authentication.js
@@ -22,6 +22,26 @@
     //});
 }
 
+function getUserSessionByAccessToken(accessToken) {
+    $.ajax({
+        url: properties.hostConnectionString + '/Session/GetUserByAccessTokenAsync',
+        type: 'GET',
+        crossDomain: true,
+        contentType: "application/json",
+        headers: {
+            'Authorization': 'Bearer '
+            + accessToken,
+            'AccessToken': accessToken
+        },
+        success: function (responseData, textStatus, jqXHR) {
+            callback(responseData);
+        },
+        error: function (responseData, textStatus, jqXHR) {
+            callback(textStatus);
+        }
+    });
+}
+
 function getLogInUserId(email, accessToken, callback) {
     $.ajax({
         url: properties.hostConnectionString + '/Account/GetUserIdByEmailAsync?email=' + email,
@@ -58,4 +78,44 @@ function getLogInUser(id, accessToken, callback) {
             callback(textStatus);
         }
     });
+}
+
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    }
+    else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+function checkCookie() {
+    var talent = getCookie("talent");
+    if (talent != null && talent != "") {
+        //alert("Welcome again " + username);
+    }
+    else {
+        //username = prompt("Please enter your username:", "");
+        //if (username != null && username != "") {
+        //    setCookie("username", username, 365);
+        //}
+    }
 }
